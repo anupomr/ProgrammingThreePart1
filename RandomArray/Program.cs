230ab8117fb574847a4ada562f6d3d2715cdeed8
Anupom Roy <anupomr@yahoo.com>
2018-06-23 17:25:25 -0400

    Count Odd Even Number

--- RandomArray/Program.cs
+++ RandomArray/Program.cs
@@ -20,15 +20,30 @@ namespace RandomArray
         }
         static void Main(string[] args)
         {
-            DisplayIntArray(new int[] { 21, 22, 23, 24, 25, 26, 27 });
+            DisplayIntArray(new int[] { 21, 22, 23, 24, 25, 26 });
+            Console.WriteLine("\nCreate a dianamic array");
+            DisplayIntArray(GenerateRandomArray(11, 11));
+            Console.WriteLine("\nOut dianamic array find Odd Even Number");
+            DisplayIntArray(CountOddEven(GenerateRandomArray(11, 11)));
+           
         }
         static void DisplayIntArray(int[] numbers)
         {
             foreach (var item in numbers)
             {
-                Console.Write($"{item,16:c}\n");
+                Console.Write($"{item,11:c}");
             }
 
         }
+        static int[] CountOddEven(int[] array)
+        {
+            int[] result = new int[2];
+            foreach (var item in array)
+            {
+                result[item % 2]++;
+            }
+            return result;
+        }
+       
     }
 }
