7dcfa949d249ab28562ad702dbb1a88d37bdccf8
Anupom Roy <anupomr@yahoo.com>
2018-06-24 01:38:25 -0400

    Entity Framework is created

---
+++ InvoiceApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApp
+{
+    class Program
+    {
+        InvoiceDBContext db = new InvoiceDBContext();
+        static void Main(string[] args)
+        {
+            var data=from a in db.Invoices
+                               select a;
+        }
+    }
+}
