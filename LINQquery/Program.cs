2fe9bc41e7cf901dfdb92743bdbe35aae4a3ff6f
Anupom Roy <anupomr@yahoo.com>
2018-06-26 00:52:43 -0400

    LINQquary Lab 1 created

---
+++ LINQquery/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQquery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
